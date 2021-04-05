2865868178228ed2817a87009f274246dd90d4ec
SunMyeong Lee <actumn814@gmail.com>
2021-04-05 22:39:18 +0900

    Fix import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 // import GameArea from './components/GameArea';
-// import TreasureHunter from './components/TreasureHunter';
-import Office from './components/Office'
+import TreasureHunter from './components/TreasureHunter';
+// import Office from './components/Office'
 
 function App() {
   return (
